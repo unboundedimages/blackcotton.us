088557407e9c7b30b39ecfcf9a70087704e65ffc
Rod Ross <rodrossdev@gmail.com>
2020-06-27 07:38:24 -0400

    updated css

--- blackcotton_us/src/SocialIcons.js
+++ blackcotton_us/src/SocialIcons.js
@@ -3,8 +3,18 @@ import React from 'react';
 class SocialIcon extends React.Component {
     render() {
         return (
-                <a href='https://www.instagram.com/blackcotton.us/' className='Social_Media'>
-                </a>            
+            <div className='SI'>
+                <div className='Social_Icons' id='IG'>
+                    <a href='https://www.instagram.com/blackcotton.us/' className='Instagram_Icon'>
+                        @blackcotton.us
+                    </a>
+                </div>
+                <div className='Social_Icons' id='FB'>
+                    <a href='https://www.facebook.com/blackcottondecor/' className='Facebook_icon'>
+                       @blackcottondecor
+                    </a>
+                </div>
+            </div>
         )
     }     
 }
